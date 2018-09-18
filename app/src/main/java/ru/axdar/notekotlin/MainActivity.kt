24ae3e7989c9931c5e0a483a365009c9f8aa892d
ildar2244 <ildar2244@gmail.com>
2018-09-18 21:02:19 +0300

    init commit

---
+++ app/src/main/java/ru/axdar/notekotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.axdar.notekotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
