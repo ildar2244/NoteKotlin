78831f87e25e12d4950c12a8a90ec398b8b8088b
ildar2244 <ildar2244@gmail.com>
2018-09-19 22:02:56 +0300

    Небольшие изменения для сборки

---
+++ app/src/main/java/ru/axdar/notekotlin/NoteApp.kt
@@ -0,0 +1,14 @@
+package ru.axdar.notekotlin
+
+import com.activeandroid.app.Application
+
+/**
+ * Created by ildar2244 on 19.09.2018.
+ */
+
+class NoteApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
